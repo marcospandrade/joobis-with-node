b2b9228d4d849d1f57d481a7502f583bdb34d905
Marcos Andrade <marcospandrade2@gmail.com>
2020-08-17 08:57:15 -0400

    Ajuste index.js

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const server = require('./server');
 
 const PORT = process.env.PORT || 3300;
 
+server.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 server.listen(PORT, () => console.log(`Server is live at localhost:${PORT}`));
